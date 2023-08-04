60d07d51a3acaff9447190d827472edb711bcccf
eyuptuylu <eyupfurkantuylu@gmail.com>
2023-08-04 23:41:45 +0300

    Initial Commit

---
+++ textFieldBootcamp/textFieldBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  textFieldBootcampApp.swift
+//  textFieldBootcamp
+//
+//  Created by Eyüp Tüylü on 4.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct textFieldBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
